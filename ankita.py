6c363a45b9a0c1649674de8eb6bbbc764a1b17e1
Ankita banerjee <137812736+TU12AB@users.noreply.github.com>
2023-06-26 22:26:48 +0530

    Add files via upload

---
+++ ankita.py
@@ -0,0 +1,12 @@
+num1=float(input("enter the no 1:"))
+num2=float(input("enter the no 2:"))
+operator=input("enter the operator:")
+if operator=='+':
+    print(num1+num2)
+elif operator=='-':
+        print(num1-num2)
+elif operator=='*':
+    print(num1*num2)
+    print(num1/num2)
+else:
+    print("invalid operator")
